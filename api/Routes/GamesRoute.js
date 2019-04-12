db3d7d0200b6283aeb816e55067c3c809a05101c
Thorben Bender <bender.thorben0@gmail.com>
2019-04-12 16:08:49 +0200

    test is not working

--- api/Routes/GamesRoute.js
+++ api/Routes/GamesRoute.js
@@ -7,8 +7,9 @@ const db = knex(knexConfig);
 
 const route = express.Router();
 
-route.use(helmet());
 route.use(express.json());
+route.use(helmet());
+
 
 route.post('/', async (req, res) => {
     try {
@@ -16,14 +17,14 @@ route.post('/', async (req, res) => {
         if (game) {
             res.status(200).json(game);
         } else {
-            res.status(405).json({ message: 'The game already exists' })
+            res.status(404).json({ message: 'The game already exists' })
         }
     } catch (err) {
         res.status(500).json(err);
     }
 })
 
-route.get('', async (req, res) => {
+route.get('/', async (req, res) => {
     try {
         games = await db('Games');
         if (games) {
--- api/Routes/GamesRoute.test.js
+++ api/Routes/GamesRoute.test.js
@@ -5,67 +5,38 @@ const route = require('./GamesRoute');
 describe('Games Route', () => {
     describe('insert data succesfull', () => {
         it('should create a new data', () => {
-            request(route)
+            return request(route)
                 .post('/games')
-                .send({ genre: 'Action-Roleplay', title: 'Borderlands 2', releaseYear: 2012 })
-                .set('Accept', 'applicaton/json')
+                .send({ genre: 'Action-Roleplay', title: 'Borderlands 3', releaseYear: 2012 })
                 .expect('Content-Type', '/json/')
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                })
         })
         it('should return the right http code post', () => {
-            request(route)
+            return request(route)
                 .post('/games')
-                .send({ genre: 'Action-Roleplay', title: 'Borderlands 2', releaseYear: 2012 })
-                .set('Accept', 'applicaton/json')
+                .send({ genre: 'Action-Roleplay', title: 'Borderlands 4', releaseYear: 2012 })
                 .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                })
         })
     })
     describe('insert data unsuccesfull', () => {
         it('should return an error', () => {
-            request(route)
+            return request(route)
                 .post('/games')
                 .send({ genre: 'Arcade', title: 'Pacman', releaseYear: 1980 })
-                .set('Accept', 'application/json')
                 .expect('Content-Type', '/json/')
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                })
         })
         it('should return the right error code', () => {
-            request(route)
-                .post('/games')
+            return request(route)
+                .post('')
                 .send({ genre: 'Arcade', title: 'Pacman', releaseYear: 1980 })
-                .set('Accept', 'application/json')
-                .expect(404)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                })
+                .expect(500)
         })
     })
     describe('get data', () => {
         it('should return the right content type', () => {
-            request(route)
+            return request(route)
                 .get('/games')
                 .expect(200)
                 .expect('Content-Type', '/json/')
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                })
-        })
-        it('should return the right http code', () => {
-            request(route)
-                .get('/games')
-                .expect(200);
         })
     })
 }) 
