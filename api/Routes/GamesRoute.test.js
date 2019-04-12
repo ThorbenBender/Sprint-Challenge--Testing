702017fb109d00ce4b1693de25fb0b2c05a33958
Thorben Bender <bender.thorben0@gmail.com>
2019-04-12 16:27:54 +0200

    testing is working

--- api/Routes/GamesRoute.test.js
+++ api/Routes/GamesRoute.test.js
@@ -1,19 +1,20 @@
 const request = require('supertest');
 
-const route = require('./GamesRoute');
+const route = require('../server');
 
 describe('Games Route', () => {
     describe('insert data succesfull', () => {
         it('should create a new data', () => {
             return request(route)
                 .post('/games')
-                .send({ genre: 'Action-Roleplay', title: 'Borderlands 3', releaseYear: 2012 })
-                .expect('Content-Type', '/json/')
+                .send({ genre: 'Action-Roleplay', title: `Borderlands ${Math.floor(Math.random() * 20000)}`, releaseYear: 2012 })
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200);
         })
         it('should return the right http code post', () => {
             return request(route)
                 .post('/games')
-                .send({ genre: 'Action-Roleplay', title: 'Borderlands 4', releaseYear: 2012 })
+                .send({ genre: 'Action-Roleplay', title: `Borderlands ${Math.floor(Math.random() * 20000)}`, releaseYear: 2012 })
                 .expect(200)
         })
     })
@@ -22,13 +23,13 @@ describe('Games Route', () => {
             return request(route)
                 .post('/games')
                 .send({ genre: 'Arcade', title: 'Pacman', releaseYear: 1980 })
-                .expect('Content-Type', '/json/')
+                .expect('Content-Type', 'application/json; charset=utf-8')
         })
         it('should return the right error code', () => {
             return request(route)
                 .post('')
                 .send({ genre: 'Arcade', title: 'Pacman', releaseYear: 1980 })
-                .expect(500)
+                .expect(404)
         })
     })
     describe('get data', () => {
@@ -36,7 +37,7 @@ describe('Games Route', () => {
             return request(route)
                 .get('/games')
                 .expect(200)
-                .expect('Content-Type', '/json/')
+                .expect('Content-Type', 'application/json; charset=utf-8')
         })
     })
 }) 
